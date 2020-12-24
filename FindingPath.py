6c7afe162321b8a822c5e2b52e9084e0e74953f8
Dhruv-Kakadiya <51689176+DZ521111@users.noreply.github.com>
2020-12-24 08:51:59 +0530

    Update FindingPath.py

--- FindingPath.py
+++ FindingPath.py
@@ -6,7 +6,7 @@ try:
     from tkinter import ttk
     from tkinter import messagebox
     import os
-    import numpy
+    import numpy as np
 except:
     #import install_requirements  # install packages
 
